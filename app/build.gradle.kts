cbe8fc2afe2de2120a030c86f72c52ace7639bee
changhu liu <liuchanghu2018@gmail.com>
2026-09-12 19:31:57 -0400

    chore: bump Notes Escape to 1.2.3 code 10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.notesescape.sdocx"
         minSdk = 27
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.2.2"
+        versionCode = 10
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
